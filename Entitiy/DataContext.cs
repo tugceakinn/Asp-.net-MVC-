ec883e2b20c8894ae91ac3980194d24aab01a5e0
tuğçe akın <akn.tue@yandex.com>
2018-12-19 16:41:16 +0300

    Abc

---
+++ Entitiy/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Abc.MvcWebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Abc.MvcWebUI.Entitiy
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("dataConnection")
+        {
+            Database.SetInitializer(new DataInitializer());
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
